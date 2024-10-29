af53da7c93d58495a8008ded73681ce609120c17
Joonas Niskala <joonas.niskala@edu.karelia.fi>
2024-10-29 19:11:42 +0200

    hintojen korjausta

--- TuotekuvastoApp/wwwroot/js/site.js
+++ TuotekuvastoApp/wwwroot/js/site.js
@@ -11,11 +11,13 @@
     });
 
     document.querySelectorAll('.product-card p:nth-child(4)').forEach(function (element) {
+        // Poista 'Price: ' ja ' €' ja muuta teksti numeroarvoksi
+        let priceText = element.textContent.replace('Price: ', '').replace(' €', '');
+        let price = parseFloat(priceText);
 
-        let price = parseFloat(element.textContent.replace('Price: ', '').replace(' €', ''));
-
-        element.textContent = `Price: ${price.toFixed(2).replace('.', ',')} €`;
-
+        // Tarkista, että hinta on numero, ja muotoile se ilman $-symbolia
+        if (!isNaN(price)) {
+            element.textContent = `Price: ${price.toFixed(2).replace('.', ',')} €`;
+        }
     });
-
 });
